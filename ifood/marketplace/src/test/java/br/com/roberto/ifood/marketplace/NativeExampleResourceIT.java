4a6c151774cf335df9fe4677e3aa2952811ec676
Carlos Roberto <carlosmedeiroslima@gmail.com>
2021-08-04 23:36:54 -0300

    Implementação Inicial do Quarkus

---
+++ ifood/marketplace/src/test/java/br/com/roberto/ifood/marketplace/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.roberto.ifood.marketplace;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
